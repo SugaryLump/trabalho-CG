6ffad76d72c32800812ce4cb83910536cecb8fa4
SugaryLump <sugarcanegaming6000@gmail.com>
2022-03-06 15:37:09 +0000

    Declared VBOController class and class methods

---
+++ includes/engine/vbo.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include <vector>
+#include "common/shapes.hpp"
+
+class VBOController {
+    private:
+        GLuint* buffers;
+        GLuint* vertices;
+
+    public:
+        VBOController() = default;
+        explicit VBOController(std::vector<Model> models);
+        
+        void drawModels();
+};
---
+++ src/engine/vbo.cpp
@@ -0,0 +1,11 @@
+#include "engine/vbo.hpp"
+
+#include <vector>
+
+VBOController::VBOController(std::vector<Model> models) {
+    //Load the VBOController's vertices and indices arrays
+}
+
+void VBOController::drawModels() {
+    //Draw the VBOController's vertex and index arrays
+}
